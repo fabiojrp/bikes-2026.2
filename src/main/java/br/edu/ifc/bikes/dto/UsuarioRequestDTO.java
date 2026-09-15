6847c77db0d9e69e4e087d9f619e821190f88426
fabiojrp <fabio.pinheiro@ifc.edu.br>
2026-09-15 11:53:12 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/br/edu/ifc/bikes/controller/UsuarioController.java

--- src/main/java/br/edu/ifc/bikes/dto/UsuarioRequestDTO.java
+++ src/main/java/br/edu/ifc/bikes/dto/UsuarioRequestDTO.java
@@ -1,6 +1,15 @@
 package br.edu.ifc.bikes.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+//@NotBlank testa o envio de ao menos 1 caracter
 public record UsuarioRequestDTO(
-    String username,
-    String password
+        @NotBlank
+        @Email(message = "O email deve ser válido")
+        String username,
+        @NotBlank(message = "A senha é obrigatória")
+        @Size(min=6, max=6)
+        String password
 ) { }
---
+++ src/main/java/br/edu/ifc/bikes/web/exception/ErrorMessage.java
@@ -0,0 +1,54 @@
+package br.edu.ifc.bikes.web.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@ToString
+public class ErrorMessage {
+
+    private String path;
+    private String method;
+    private int status; //código http
+    private String statusMessage; //mensagem do status http
+    private String message;
+
+    //indica que o campo errors será incluído na resposta JSON apenas se não for nulo
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Map<String, String> errors;
+
+    public ErrorMessage(){}
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message){
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusMessage = status.getReasonPhrase();
+        this.message = message;
+    }
+
+    //BindingResult é uma interface do String que contém os erros de validação
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult result){
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusMessage = status.getReasonPhrase();
+        this.message = message;
+        addErrors(result);
+    }
+
+    private void addErrors(BindingResult result) {
+        this.errors = new HashMap<>();
+        for (FieldError fieldError : result.getFieldErrors()){
+            this.errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
